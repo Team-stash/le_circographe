96758b1e1f145188f67a86150bcc8903b258a890
hellijah <thierry.corlieto@gmail.com>
2024-12-04 16:42:16 +0100

    update migration removed payment lock in usermemberships

--- db/migrate/20241204101104_create_user_memberships.rb
+++ db/migrate/20241204101104_create_user_memberships.rb
@@ -3,7 +3,7 @@ class CreateUserMemberships < ActiveRecord::Migration[8.0]
     create_table :user_memberships do |t|
       t.references :user, null: false, foreign_key: true
       t.references :subscription_type, null: false, foreign_key: true
-      t.references :payment, null: false, foreign_key: true
+      t.references :payment, foreign_key: true
       t.boolean :status
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_04_143307) do
   create_table "user_memberships", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "subscription_type_id", null: false
-    t.integer "payment_id", null: false
+    t.integer "payment_id"
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
