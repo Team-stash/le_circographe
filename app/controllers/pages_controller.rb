a3e731b9adbebf403ae5dde1516c4aa39edd96cb
Elie-Kauptairr <elieflo64@gmail.com>
2024-12-06 10:42:01 +0100

    links navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
   allow_unauthenticated_access
-  
+
   def show
   end
 
   def about
+  end
+
+  def le_cirque
+  end
+
+  def le_graff
+  end
+
+  def le_lieu
   end
 end
