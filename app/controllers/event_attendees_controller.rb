047cb1cb0f2e6fb245662f163c8099ac3b2c82f2
RAMASSAMY Luc <luko974@gmail.com>
2024-12-12 17:13:13 +0100

    implemented interest of user in events

---
+++ app/controllers/event_attendees_controller.rb
@@ -0,0 +1,18 @@
+class EventAttendeesController < ApplicationController
+  def create
+    event_id = params[:id]
+    if authenticated?
+      EventAttendee.create!(user_id: Current.user.id, event_id: event_id, payment_id: 1)  #placeholder value for payment_id
+    end
+    redirect_to event_path(event_id)
+  end
+
+  def destroy
+    if authenticated?
+      event_id = params[:id]
+      eattendee = EventAttendee.where("user_id = #{Current.user.id} AND event_id = #{event_id}")
+      EventAttendee.destroy eattendee
+    end
+    redirect_to event_path(event_id)
+  end
+end
