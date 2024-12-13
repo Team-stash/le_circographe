5ee2cf1d587e0548c24293354ab60864e0988f98
AkaKwak <christophe.alaterre@gmail.com>
2024-12-13 08:13:11 +0100

    opening hours now modified from backend to frontend

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,18 @@ module Admin
   class DashboardController < BaseController
 
     def index
+      @opening_hours = Rails.cache.fetch("opening_hours") || DEFAULT_OPENING_HOURS
     end
+
+    DEFAULT_OPENING_HOURS = {
+      lundi: "Fermé",
+      mardi: "14:00 - 22:00",
+      mercredi: "14:00 - 22:00",
+      jeudi: "14:00 - 22:00",
+      vendredi: "14:00 - 22:00",
+      samedi: "14:00 - 22:00",
+      dimanche: "14:00 - 22:00"
+    }.freeze
+
   end
 end
