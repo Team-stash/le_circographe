fd22288932372a8127663056a63a70e87b46522b
AkaKwak <christophe.alaterre@gmail.com>
2024-12-13 23:25:09 +0100

    Hadrienhelper

---
+++ app/controllers/admin/events_controller.rb
@@ -0,0 +1,33 @@
+module Admin
+  class EventsController < BaseController
+    def new
+      @event = Event.new
+      if !Current.user.has_privileges?
+        redirect_to root_path, alert: "Réservé aux administrateurs"
+      end
+    end
+    def create
+      @event = Event.new(event_params)
+      @event.creator = User.find Current.user.id
+      @event.save!
+      redirect_to root_path, notice: "Evenement créé avec succès"
+    end
+    def edit
+      @event = Event.find params[:id]
+      if !Current.user.has_privileges?
+        redirect_to root_path, alert: "Réservé aux administrateurs"
+      end
+    end
+    def update
+      @event = Event.find params[:id]
+      if @event.update(event_params)
+        redirect_to event_path, notice: "Evenement modifié avec succès"
+      end
+    end
+    private
+    def event_params
+      params.fetch(:event, {})
+      params.require(:event).permit(:title, :upper_description, :middle_description, :bottom_description, :location, :date)
+    end
+  end
+end
--- app/helpers/opening_hours_helper.rb
+++ app/helpers/opening_hours_helper.rb
@@ -1,2 +1,13 @@
 module OpeningHoursHelper
+  def default_opening_hours
+  {
+    lundi: "Fermé",
+    mardi: "14:00 - 22:00",
+    mercredi: "14:00 - 22:00",
+    jeudi: "14:00 - 22:00",
+    vendredi: "14:00 - 22:00",
+    samedi: "14:00 - 22:00",
+    dimanche: "14:00 - 22:00"
+  }.freeze
+  end
 end
