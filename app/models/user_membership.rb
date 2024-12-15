698339d39b934fb909673f6745a5b2df6a01cb78
hellijah <thierry.corlieto@gmail.com>
2024-12-15 16:33:45 +0100

    db working with new basic member

--- app/models/user_membership.rb
+++ app/models/user_membership.rb
@@ -1,5 +1,7 @@
 class UserMembership < ApplicationRecord
   belongs_to :user
+  belongs_to :subscription_type
+  belongs_to :payment, optional: true
 
   # has_many :payments, dependent: :destroy
   has_many :training_attendees
